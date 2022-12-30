f3adc8904a924cbfa741b459160ee4565101ba0a
Josip Jurenić <josip.jurenic12@gmail.com>
2022-12-30 03:21:44 +0100

    change config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
-const runtimeCaching = require('next-pwa/cache')
 
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  runtimeCaching,
-})
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
 
 module.exports = withPWA({
   reactStrictMode: true,
-})
+});
